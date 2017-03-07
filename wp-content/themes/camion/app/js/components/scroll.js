44d4d58576b9bb95362f0eb07e271d6098d5db9a
Trapdafifou <t.marsal@mutum.fr>
2017-03-07 11:12:37 +0100

    [ADD] animation Home, scrollMagic(lib)

--- wp-content/themes/camion/app/js/components/scroll.js
+++ wp-content/themes/camion/app/js/components/scroll.js
@@ -10,34 +10,8 @@ module.exports = function () {
     function scroll() {
         //Enter all elements here that you want to be scroll and set their offset
 
-        var elementsTab = [
-            // {name: '.cover', offset: 0},
-            {name: '.section-histoire', offset: 200},
-            {name: '.section-trouver', offset: 200}
-        ];
-
-        function topPosition(element) {
-            return $(element).offset().top;
-        }
-
-        //fade your text
-        function fadeText (text) {
-            text.css('opacity', '1')
-        }
-
-        var controller = new ScrollMagic.Controller();
-        for (var i = 0; i <= elementsTab.length - 1; i++) {
-
-            new ScrollMagic.Scene({
-                duration: 2000,
-                offset: topPosition(elementsTab[i].name) - elementsTab[i].offset
-            })
-
-                .setPin(elementsTab[i].name)
-                .addTo(controller)
-                // .on('start', fadeText($(elementsTab[i].name)))
-        }
     }
+
     $(document).ready(function () {
         scroll()
     });
